426ce62e4d0c4ce9f538e0be3e9308c48a3d0479
Sahadat Hosen <ssar96540@gmail.com>
2024-09-02 22:00:57 +0600

    Complete navbar responsive issue

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,41 +10,53 @@ const Header = () => {
   ]
   console.log(NavItem);
   return (
-    <div className="h-[226px]  space-y-6 pt-4">
+    <div className="md:h-[226px] md:space-y-6 md:pt-4">
       {/*top nav  */}
-      <div className="flex  md:flex justify-center items-center gap-5">
-        <h1 className="text-smTexColor md:text-xl text-sm">(800) 120 2387</h1>
+      <div className="flex  md:flex justify-center items-center gap-5 md:mt-0 mt-2">
+        {/* number */}
+        <h1 className="text-smTexColor md:flex hidden  md:text-xl text-sm">(800) 120 2387</h1>
         {/* input */}
-      <label className="relative  flex justify-center items-center">
+      <label className="relative  md:flex hidden justify-center items-center">
       <input
           type="text"
-          className="text-white md:w-[900px]    rounded-full border border-gray-400  bg-primary md:px-6 px-2 md:p-1"
+          className="text-white md:w-[900px]  w-[300px]  rounded-full border border-gray-400  bg-primary md:px-6 px-2 md:p-1"
           placeholder="Search for Gold jewellery, Diamond Jewellery and more ..."
         />
 
         <IoSearch className="text-smTexColor absolute right-6 mt-0    hover:text-textYellow cursor-pointer"  />
       </label>
         {/* icon */}
-        <div className="text-textYellow md:text-2xl flex  md:gap-x-3 gap-x-2">
+        <div className="text-textYellow md:text-2xl md:flex hidden md:gap-x-3 gap-x-2">
           <FaFacebookF />
           <FaTwitter />
           <IoLogoInstagram />
         </div>
       </div>
       {/* Center nav */}
-      <div className="text-white flex justify-center items-center uppercase text-lg font-fell gap-x-3  ">
+      
+      
+     <div className="text-white flex justify-center items-center uppercase text-lg font-fell gap-x-3  ">
         <h1>
-          {" "}
           <span className="md:text-4xl">J</span>ewelry{" "}
         </h1>
 
         <img src="/src/image/logo.png" alt="" />
 
         <h1>
-          {" "}
           <span className="md:text-4xl">E</span>mpire
         </h1>
       </div>
+      {/* for sm input */}
+      <label className="relative md:hidden flex  justify-center items-center mt-2">
+      <input
+          type="text"
+          className="text-white w-[350px]    rounded-full border border-gray-400  bg-primary md:px-6 px-2 md:p-1"
+          placeholder="Search for Gold .."
+        />
+
+        <IoSearch className="text-smTexColor absolute right-10 mt-0    hover:text-textYellow cursor-pointer"  />
+      </label>
+   
       {/* Bottom nav */}
 
       <nav className="flex items-center justify-center   px-4 py-2 text-white    ">
